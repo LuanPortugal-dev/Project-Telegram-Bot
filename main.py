3a57a9eb30d5e58c8e7ee517548464da22723886
Luan Portugal <luanportugal.profissional.tec@gmail.com>
2021-01-27 00:22:45 -0300

    Initializing the connection with API

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import request
 import time
 import json
 import os
+
+# Inicializando a conexão com API
+class TelegramBot(self):
+    def __init__(self):
+        token = '1617152764:AAEfEZThYahcfARvQDSy7em-WlQr8gFfmNg'
+        self.url_base = f'https://api.telegram.org/bot{token}/'
